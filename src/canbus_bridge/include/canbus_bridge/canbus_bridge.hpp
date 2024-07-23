9e773ff1f68819549ac07ba54e38fbf1c0c1ddd8
Edoardo Torrini <edoardo.torrini@gmail.com>
2024-07-23 10:04:18 +0200

    add canbus_bridge - need to add control for mutual exclusion for the read/write in socket

---
+++ src/canbus_bridge/include/canbus_bridge/canbus_bridge.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <mmr_kria_base/edf_node.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/exceptions.hpp>
+#include <can_msgs/msg/frame.hpp>
+
+#include <sys/syscall.h>
+#include <linux/sched.h>
+#include <sched.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include <sys/socket.h>
+#include <mutex>
+
+#include <string.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+
+#define gettid() syscall(SYS_gettid)
+
+class CANBusBridge : public EDFNode
+{
+
+    private:
+
+        std::string m_sInterface, m_sTopicTx, m_sTopicRx;
+        int m_nBitrate, m_nPeriod, m_nWCET, m_nDeadline, m_nMaxMsgs;
+        bool m_bDebug;
+
+        void loadParameters();
+
+        /* Subscriber for CANBus Msg */
+        rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr m_subCANRx;
+        void msgCANBusRxCallback(const can_msgs::msg::Frame::SharedPtr msg);
+
+        /* Publisher for CANBus Msg */
+        rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr m_pubCANBusTx;
+
+        int m_nSocket;
+        struct ifreq m_ifr;
+        struct sockaddr_can m_addr;
+
+        void connectCANBus();
+
+    public:
+
+        CANBusBridge();
+        ~CANBusBridge() { close(this->m_nSocket); };
+
+        void readMsgFromCANBus();
+
+};
