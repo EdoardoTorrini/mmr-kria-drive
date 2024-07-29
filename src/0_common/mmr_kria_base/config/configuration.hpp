40636696b73a818d5a0876d5e448f48260a6845e
Edoardo Torrini <edoardo.torrini@gmail.com>
2024-07-29 20:31:52 +0200

    clear and refactor node mmr_kria_base, only contain custom msg and config file

--- src/0_common/mmr_kria_base/config/configuration.hpp
+++ src/0_common/mmr_kria_base/config/configuration.hpp
@@ -1,29 +1,5 @@
 #pragma once
 
-#include <sys/syscall.h>
-#include <linux/sched.h>
-#include <sched.h>
-
-#include <linux/types.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <string.h>
-#include <cstdint>
-
-#define gettid() syscall(SYS_gettid)
-
-// Necessary for EDF scheduling
-struct sched_attr {
-    uint32_t size;
-    uint32_t sched_policy;
-    uint64_t sched_flags;
-    int32_t sched_nice;
-    uint32_t sched_priority;
-    uint64_t sched_runtime;
-    uint64_t sched_deadline;
-    uint64_t sched_period;
-};
-
 namespace RES {
 
     enum MMR_CAN_MSG_ID {
